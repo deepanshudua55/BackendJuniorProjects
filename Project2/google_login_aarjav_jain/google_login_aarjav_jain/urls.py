747a201a894d3481b71e1bf7b993e4a0ffb437ed
Aarjav Jain <aarjav00@gmail.com>
2018-11-17 03:29:05 +0530

    Google Login Completed

--- Project2/google_login_aarjav_jain/google_login_aarjav_jain/urls.py
+++ Project2/google_login_aarjav_jain/google_login_aarjav_jain/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from login import views
-from django.conf.urls import url, include
+from django.urls import path, include
+from django.contrib.auth import views
+from login.views import home
+from django.conf.urls import url
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', views.login, name='login'),
-    #url(r'^logout/$', views.logout, name='logout'),
-    #url(r'^auth/', include('login.urls', namespace='login')),
-    url(r'^$', views.home, name='home'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^auth/', include('social_django.urls', namespace='social')),
+    url(r'^$',  home, name='home'),
 ]
